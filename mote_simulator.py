cfb5da964ce1a9c4d093c7b570b168ab3d192b6e
Kristoffer Gard Osen <Kristoffer Gard Osen>
2017-08-23 19:00:36 +0200

    sdf

---
+++ mote_simulator.py
@@ -0,0 +1,23 @@
+import sched, time
+import csv
+import requests
+import json
+
+
+print('Posting every 30min to http://192.81.221.165:8085/api/plant')
+with open('data.json') as data_file:    
+    data = json.load(data_file)
+
+i = 0
+s = sched.scheduler(time.time, time.sleep)
+
+def do_something(sc,iterator): 
+    r = requests.post('http://192.81.221.165:8085/api/plant', auth=('user', 'pass'), data=data[iterator])
+    iterator = iterator + 1
+    s.enter(1, 1, do_something, (sc,iterator))
+
+
+s.enter(1, 1, do_something, (s,i))
+s.run()
+
+
