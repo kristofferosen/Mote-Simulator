cf456ccb9663d26f1ce275bba9413e1083dda63f
Kristoffer Gard Osen <Kristoffer Gard Osen>
2017-09-04 14:26:51 +0200

    evigforalltid

--- mote_simulator.py
+++ mote_simulator.py
@@ -12,14 +12,25 @@ i = 0
 s = sched.scheduler(time.time, time.sleep)
 
 def do_something(sc,iterator): 
+
+	print("Posting")
     r = requests.post('http://192.81.221.165:8085/api/plant', auth=('user', 'pass'), data=data[iterator])
     iterator = iterator + 1
     if(i==701):
     	i = 0
-    s.enter(1, 1, do_something, (sc,iterator))
+    r = requests.post('http://192.81.221.165:8085/api/plant', auth=('user', 'pass'), data=data[iterator])
+    iterator = iterator + 1
+    if(i==701):
+    	i = 0
+    r = requests.post('http://192.81.221.165:8085/api/plant', auth=('user', 'pass'), data=data[iterator])
+    iterator = iterator + 1
+    if(i==701):
+    	i = 0
+    
+    s.enter(1800, 1, do_something, (sc,iterator))
 
 
-s.enter(1, 1, do_something, (s,i))
+s.enter(1800, 1, do_something, (s,i))
 s.run()
 
 
